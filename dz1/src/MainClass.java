8b5827d9c0a78532fe9cb2ca92032089e0d39cbe
oirikata <oiri.kata@gmail.com>
2019-03-30 10:03:31 +0300

    создан метод, принимающий на вход два числа, и проверяющий что их сумма лежит в пределах от 10 до 20(включительно)

--- dz1/src/MainClass.java
+++ dz1/src/MainClass.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class MainClass {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         byte bt = 1;
         short sh = 1;
         int i = 1;
@@ -11,10 +14,25 @@ public class MainClass {
         boolean bl = true;
         String str = "DZ #1";
         System.out.println(calculate(5, 4, 10, 3));
+        System.out.println("введите первое число для проверки");
+        int a=scanner.nextInt();
+        System.out.println("введите второе число для проверки");
+        int b=scanner.nextInt();
+        if (ifSumm1020(a,b) ==true)
+            System.out.println("Сумма чисел лежит в диапазоне от 10 до 20");
+        else System.out.println("Сумма чисел не лежит в диапазоне от 10 до 20");
 
     }
 
     public static double calculate(double a, double b, double c, double d) {
         return a * (b + (c / d));
+    }
+    public static boolean ifSumm1020 (int a, int b){
+        boolean result = false;
+        if (a+b >=10 && a+b <=20) {
+            result=true;
+        }
+        else result=false;
+        return result;
     }
 }
