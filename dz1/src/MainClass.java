760f900137903ac43de0a0f31c636500519e9153
oirikata <oiri.kata@gmail.com>
2019-03-30 09:41:22 +0300

    созданы переменные всех изученных типов

--- dz1/src/MainClass.java
+++ dz1/src/MainClass.java
@@ -1,6 +1,16 @@
 public class MainClass {
 }
 public static void main (String[] args) {
+    byte bt=1;
+    short sh=1;
+    int i=1;
+    long lg=1;
+    float fl=10.3f;
+    double db=10.5;
+    char ch=15;
+    boolean bl=true;
+    String str="DZ #1";
     
+
 
 }
