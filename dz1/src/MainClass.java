6e802b94d4034dcedee48bb65a8e77c7f8e6209b
oirikata <oiri.kata@gmail.com>
2019-03-30 10:12:07 +0300

    создан метод, который печатает в консоль положительное ли число передали, или отрицательное;

--- dz1/src/MainClass.java
+++ dz1/src/MainClass.java
@@ -14,25 +14,35 @@ public class MainClass {
         boolean bl = true;
         String str = "DZ #1";
         System.out.println(calculate(5, 4, 10, 3));
-        System.out.println("введите первое число для проверки");
+        System.out.println("введите первое число для проверки сумм");
         int a=scanner.nextInt();
-        System.out.println("введите второе число для проверки");
+        System.out.println("введите второе число для проверки сумм");
         int b=scanner.nextInt();
         if (ifSumm1020(a,b) ==true)
             System.out.println("Сумма чисел лежит в диапазоне от 10 до 20");
         else System.out.println("Сумма чисел не лежит в диапазоне от 10 до 20");
-
+        System.out.println("введите первое число для проверки положительное оно или отрицательное");
+        int number1=scanner.nextInt();
+        ifNumberAboveZero(number1);
     }
 
     public static double calculate(double a, double b, double c, double d) {
         return a * (b + (c / d));
     }
     public static boolean ifSumm1020 (int a, int b){
-        boolean result = false;
+        boolean result;
         if (a+b >=10 && a+b <=20) {
             result=true;
         }
         else result=false;
         return result;
+    }
+    public static void ifNumberAboveZero (int a) {
+        boolean result;
+        if (a>=0) result=true;
+        else result=false;
+        if (result==true) System.out.println("Введенное число положительное");
+        else System.out.println("Введенное число отрицательное");
+
     }
 }
