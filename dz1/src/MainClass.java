34c136ee655c99a5b717e9b6ba79ee0fb5d16ae5
oirikata <oiri.kata@gmail.com>
2019-03-30 09:51:05 +0300

    создан метод вычисляющий выражение a * (b + (c / d)) и возвращающий результат

--- dz1/src/MainClass.java
+++ dz1/src/MainClass.java
@@ -1,16 +1,20 @@
 public class MainClass {
-}
-public static void main (String[] args) {
-    byte bt=1;
-    short sh=1;
-    int i=1;
-    long lg=1;
-    float fl=10.3f;
-    double db=10.5;
-    char ch=15;
-    boolean bl=true;
-    String str="DZ #1";
-    
 
+    public static void main(String[] args) {
+        byte bt = 1;
+        short sh = 1;
+        int i = 1;
+        long lg = 1;
+        float fl = 10.3f;
+        double db = 10.5;
+        char ch = 15;
+        boolean bl = true;
+        String str = "DZ #1";
+        System.out.println(calculate(5, 4, 10, 3));
 
+    }
+
+    public static double calculate(double a, double b, double c, double d) {
+        return a * (b + (c / d));
+    }
 }
